177ed38518cde805e6741f193a1d14af74edf2dc
Nazima <38064594+nkcrazy@users.noreply.github.com>
2022-01-15 07:12:42 +0500

    news

---
+++ assets/js/javascript.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function () {
+    $('.special-offers_slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+    });
+});
